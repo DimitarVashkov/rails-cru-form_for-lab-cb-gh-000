ca5032db1060708aa2f44741e4a2e7809feedb02
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-10 01:34:52 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,12 +12,16 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    
+    @artist = Artist.new
   end
 
   def edit
     @artist = Artist.find(params[:id])
   end
 
+  private
 
+  def artist_params
+    params.require(:artist).permit(:name,:bio)
+  end
 end
