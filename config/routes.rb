c7f253e46bc34dceaac342fb17a169b620657e09
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-10 01:29:49 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'artists/index'
+
+  get 'artists/show'
+
+  get 'artists/new'
+
+  get 'artists/edit'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
