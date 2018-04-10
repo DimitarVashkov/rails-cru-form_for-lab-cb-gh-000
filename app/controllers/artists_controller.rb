01580f3569a9cf184241d68d129d73384f7e01db
Dimitar Vashkov <dimitarvashkov@icloud.com>
2018-04-10 01:35:20 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -12,7 +12,12 @@ class ArtistsController < ApplicationController
   end
 
   def create
-    @artist = Artist.new
+    @artist = Artist.new(artist_params)
+    if @artist.save 
+      redirect_to @artist
+    else
+      render :new
+    end
   end
 
   def edit
